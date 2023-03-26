570a700e9aa1b75bccffe902171e834b0aa45030
bakhtiyor-creator <bsayfiddinov777@gmail.com>
2023-03-26 13:29:50 +0500

    finish

--- IMIXON 3/2.js
+++
@@ -1,2 +0,0 @@
-let str =  "Satrdagi eng uzun va eng qisqa so'zlarni uzunligini ayirmasini qaytaruvchi funksiya tuzing";
-    
--- js/dark_light.js
+++
@@ -1,6 +0,0 @@
-const toogle = document.getElementById('tooglDark');
-const body = document.querySelector('body');
-
-toogle.addEventListener('click', function() {
-    this.classList.toogle('bi-moon');
-})
